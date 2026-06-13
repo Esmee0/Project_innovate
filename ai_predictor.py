1b677e1b4dbf5602fac01beb83e446f1be53b721
Esmee <esmee.moller@student.nhlstenden.com>
2026-06-13 15:46:59 +0200

    ai_predictor.py

--- ai_predictor.py
+++ ai_predictor.py
@@ -13,4 +13,67 @@ class DurationPredictor:
 
     
     def train(self, assignment, worklogs):
+        if not assignment or not worklogs:
+            return False
         
+        X_text = []
+        X_numerical = []
+        y = []
+
+        for assignment in assignments:
+            logs = [wl for wl in worklogs if wl.assignment_id == assignment.id]
+
+            if not logs:
+                continue 
+
+
+            total_hours = sum(wl.hours_done for wl in logs)
+
+            if  total_hours <= 0:
+                continue
+
+            X_text.append(assignment.title)
+
+            days_available = (assignment.due_date - assignment.start_date).days
+
+            days_since_start - (date.today() - assignment.start_date).days
+
+            X_numerical.append([days_available, day_since_start])
+            y.append(total_hours)
+
+        if len(X_text) < 2:
+            return False
+        
+        X_text_features = self.vectorizer.fit_transform(X_text).toarray()
+
+        X= np.hstack([X_text_features, X_numerical])
+
+        self.model.fit(x, y)
+        self.is_trained = True
+        return True
+    
+    
+    def predict(self, title, start_date, due_date):
+        if not self.is_trained:
+            return None
+        try:
+            X_text_features = self.vectorizer.transform([title]).toarray()
+
+            days_available = (due_date - start_date).days
+            days_since_start = (date.today() - start_date).days
+
+            X_numerical = np.array([[days_available, days_since_start]])
+
+            X = np.hstack([X_text_features, X_numerical])
+
+            prediction = self.model.predict(X)[0]
+
+            return max(0.5, prediction)
+        
+        except Exception as e:
+            print(f"prediction error: {e}")
+            return None
+        
+predictor = DurationPredictor
+
+
